906508dce6d64bd2b031ab966d08bce701207c4a
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2021-10-31 20:59:17 +0200

    Update game.js

--- game.js
+++ game.js
@@ -2,18 +2,20 @@ const game = document.getElementById("gameBoard")
 var buttonId = 1
 var seconds = 0
 var time = document.getElementById("timer")
-const buttonBoard = []
+var buttonBoard = []
 const numberOfBombs = 30
 const validButtons = 370
-const bombs = []
+var bombs = []
+var bombsNear = []
 createBoard()
 generateBombs()
 checkForBomb()
 addMark()
+addBombPosition()
 function createBoard() {
     var k = 1;
     for(var i = 1; i <= 20; ++i) {
-        for (var j = 1; j <= 20; ++j){
+        for (var j = 1; j <= 20; ++j) {
             var button = document.createElement("button")
             button.setAttribute("id", k)
             button.setAttribute("class", "buttons")
@@ -28,7 +30,7 @@ function generateBombs() {
     for(var i = numberOfBombs; i > 0; --i) {
         var randomNumber = Math.floor(Math.random() * 400 - 1 + 1)
         if(!(bombs.includes(randomNumber)) && randomNumber != 0) {
-            bombs.push(randomNumber)
+            bombs.push(parseInt(randomNumber))
         } else {
             ++i
         }
@@ -47,12 +49,10 @@ function getButtonId(button) {
 
 function checkForBomb() {
     const allButtons = Array.from(game.querySelectorAll(".buttons"))
-    console.log(allButtons)
-    console.log(bombs)
     allButtons.forEach(button => {
         button.addEventListener('click', e => {
             var isBomb = false
-            var buttonId = getButtonId(button)
+            var buttonId = parseInt(getButtonId(button)) 
             for(var i = 1; i < bombs.length; ++i) {
                 if(buttonId == bombs[i]) {
                     isBomb = true;
@@ -64,7 +64,68 @@ function checkForBomb() {
                     document.getElementById(bombs[i]).style.backgroundColor = 'red'
                 }
             } else {
-                document.getElementById(buttonId).disabled = true
+                var numberOfBombs = checkNumberOfBombs(buttonId, bombs)
+                if(numberOfBombs != 0) {
+                    document.getElementById(buttonId).innerHTML = numberOfBombs
+                } else {
+                    var copyOfId1 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId1] == 0){
+                        document.getElementById(copyOfId1).disabled = true
+                        ++copyOfId1
+                    }
+                    document.getElementById(copyOfId1).innerHTML = buttonBoard[copyOfId1]
+                    document.getElementById(copyOfId1).disabled = true
+                    var copyOfId2 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId2] == 0){
+                        document.getElementById(copyOfId2).disabled = true
+                        --copyOfId2
+                    }
+                    document.getElementById(copyOfId2).innerHTML = buttonBoard[copyOfId2]
+                    document.getElementById(copyOfId2).disabled = true
+                    var copyOfId3 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId3] == 0){
+                        document.getElementById(copyOfId3).disabled = true
+                        copyOfId3 -= 20
+                    }
+                    document.getElementById(copyOfId3).innerHTML = buttonBoard[copyOfId3]
+                    document.getElementById(copyOfId3).disabled = true
+                    var copyOfId4 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId4] == 0){
+                        document.getElementById(copyOfId4).disabled = true
+                       copyOfId4 += 20
+                    }
+                    document.getElementById(copyOfId4).innerHTML = buttonBoard[copyOfId4]
+                    document.getElementById(copyOfId4).disabled = true
+                    var copyOfId5 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId5] == 0){
+                        document.getElementById(copyOfId5).disabled = true
+                        copyOfId5 -= 19
+                    }
+                    document.getElementById(copyOfId5).innerHTML = buttonBoard[copyOfId5]
+                    document.getElementById(copyOfId5).disabled = true
+                    var copyOfId6 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId6] == 0){
+                        document.getElementById(copyOfId6).disabled = true
+                        copyOfId6 += 19
+                    }
+                    document.getElementById(copyOfId6).innerHTML = buttonBoard[copyOfId6]
+                    document.getElementById(copyOfId6).disabled = true
+                    var copyOfId7 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId7] == 0){
+                        document.getElementById(copyOfId7).disabled = true
+                        copyOfId7 -= 21
+                    }
+                    document.getElementById(copyOfId7).innerHTML = buttonBoard[copyOfId7]
+                    document.getElementById(copyOfId7).disabled = true
+                    var copyOfId8 = parseInt(buttonId) 
+                    while(buttonBoard[copyOfId8] == 0){
+                        document.getElementById(copyOfId8).disabled = true
+                        copyOfId8 += 21
+                    }
+                    document.getElementById(copyOfId8).innerHTML = buttonBoard[copyOfId8]
+                    document.getElementById(copyOfId8).disabled = true
+                }
+                 
             }
         })
     })  
@@ -80,3 +141,26 @@ function addMark() {
     })
 }
 
+function checkNumberOfBombs(buttonId, bombs) {
+    var totalBombsNear = 0
+    for(var i = 0; i < bombs.length; ++i) {
+        if(bombs[i] == buttonId + 1 || bombs[i] == buttonId + 19 || bombs[i] == buttonId + 20 || bombs[i] == buttonId + 21
+        ||bombs[i] == buttonId - 1 || bombs[i] == buttonId - 19 || bombs[i] == buttonId - 20 || bombs[i] == buttonId - 21 ) {
+            ++totalBombsNear;
+        }
+    }
+    return totalBombsNear
+}
+
+function addBombPosition() {
+    for(var i = 1; i <= 400; ++i) {
+        var bombsNear = checkNumberOfBombs(i, bombs)
+        if(bombsNear != 0) {
+            buttonBoard[i] = parseInt(bombsNear)
+        }
+        else {
+            buttonBoard[i] = parseInt(0)
+        }
+    }
+    console.log(buttonBoard)
+}
