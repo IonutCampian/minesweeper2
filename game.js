f97234c64789993d1c74bf3971be69155b5e5b05
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2021-10-31 12:29:57 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,40 +1,82 @@
 const game = document.getElementById("gameBoard")
-var buttonId = 1;
-var seconds = 0;
+var buttonId = 1
+var seconds = 0
 var time = document.getElementById("timer")
-
-createBoard();
-
+const buttonBoard = []
+const numberOfBombs = 30
+const validButtons = 370
+const bombs = []
+createBoard()
+generateBombs()
+checkForBomb()
+addMark()
 function createBoard() {
     var k = 1;
     for(var i = 1; i <= 20; ++i) {
         for (var j = 1; j <= 20; ++j){
-            var button = document.createElement("button");
-            button.setAttribute("id", "btns");
-            button.setAttribute("number", k)
-            button.style.gridRowStart = i;
-            button.style.gridColumnStart= j;
-            game.appendChild(button);
-            ++k;
+            var button = document.createElement("button")
+            button.setAttribute("id", k)
+            button.setAttribute("class", "buttons")
+            game.appendChild(button)
+            buttonBoard.push(k)
+            ++k
+        } 
+    }
+}
+
+function generateBombs() {
+    for(var i = numberOfBombs; i > 0; --i) {
+        var randomNumber = Math.floor(Math.random() * 400 - 1 + 1)
+        if(!(bombs.includes(randomNumber)) && randomNumber != 0) {
+            bombs.push(randomNumber)
+        } else {
+            ++i
         }
     }
-    addEventListener();
 }
-
-function addEventListener() {
-    var gameButtons = document.querySelectorAll("container");
-    gameButtons.forEach(button =>{
-         var btn = document.getElementById(btns);
-         btn.addEventListener("click", e =>{ 
-            btn.innerHTML = btn.getAttribute("Number")
-         })
-    })
-}
-
 
 function passTime() {
-    seconds += 1;
-    document.getElementById("timer").innerText = seconds;
+    seconds += 1
+    document.getElementById("timer").innerText = seconds
+    console.log(seconds)
 }
 
+function getButtonId(button) {
+    return button.getAttribute("id")
+}
+
+function checkForBomb() {
+    const allButtons = Array.from(game.querySelectorAll(".buttons"))
+    console.log(allButtons)
+    console.log(bombs)
+    allButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            var isBomb = false
+            var buttonId = getButtonId(button)
+            for(var i = 1; i < bombs.length; ++i) {
+                if(buttonId == bombs[i]) {
+                    isBomb = true;
+                    break;
+                }
+            }
+            if(isBomb) {
+                for(var i = 1; i < bombs.length; ++i) {
+                    document.getElementById(bombs[i]).style.backgroundColor = 'red'
+                }
+            } else {
+                document.getElementById(buttonId).disabled = true
+            }
+        })
+    })  
+}
+
+function addMark() {
+    const allButtons = game.querySelectorAll(".buttons")
+    allButtons.forEach(button => {
+        button.addEventListener("contextmenu", e =>{
+            var buttonId = getButtonId(button)
+            document.getElementById(buttonId).style.backgroundColor = 'yellow'
+        })
+    })
+}
 
