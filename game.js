8dff29358cde782160eac1b6586abff213c7f313
IonutCampian <87191750+IonutCampian@users.noreply.github.com>
2021-11-04 23:12:29 +0200

    Update game.js

--- game.js
+++ game.js
@@ -7,15 +7,18 @@ const numberOfBombs = 30
 const validButtons = 370
 var bombs = []
 var bombsNear = []
-const allPossibleButtons = []
+let allPossibleButtons = []
+let flags= []
 var left = [21,41,61,81,101,121,141,161,181,201,221,241,261,281,301,321,341,361]
 var right =[40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340,360,380]
 var up = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 var down = [382,383,384,385,386,387,388,389,390,391,392,393,394,395,396,397,398,399]
 createBoard()
 generateBombs()
-checkForBomb()
 addMark()
+console.log(bombs)
+console.log(allPossibleButtons)
+console.log(bombsNear)
 function createBoard() {
     for(var i = 1; i <= 400; ++i) {
             var button = document.createElement("button")
@@ -29,7 +32,7 @@ function generateBombs() {
     for(var i = numberOfBombs; i > 0; --i) {
         var randomNumber = Math.floor(Math.random() * 400 - 1 + 1)
         if(!(bombs.includes(randomNumber)) && randomNumber != 0) {
-            bombs.push(parseInt(randomNumber))
+            bombs[i] = randomNumber
         } else {
             ++i
         }
@@ -37,33 +40,7 @@ function generateBombs() {
     
     for( let i = 1; i <= 400; ++i) {
         var total = 0
-        if(i != 1 && i != 400 && i != 381 && i != 20 && !bombs.includes(i) && !left.includes(i) && !right.includes(i) &&
-        !up.includes(i) && !down.includes(i)) {
-            if(bombs.includes(i + 1)){
-                ++total
-            }
-            if(bombs.includes(i + 19)){
-                ++total
-            }
-            if(bombs.includes(i + 20)){
-                ++total
-            }
-            if(bombs.includes(i + 21)){
-                ++total
-            }
-            if(bombs.includes(i - 1)){
-                ++total
-            }
-            if(bombs.includes(i - 19)){
-                ++total
-            }
-            if(bombs.includes(i - 20)){
-                ++total
-            }
-            if(bombs.includes(i - 21)){
-                ++total
-            }
-        } else if (left.includes(i)){
+        if (left.includes(i)){
             if(bombs.includes(i + 1)){
                 ++total
             }
@@ -83,7 +60,7 @@ function generateBombs() {
             if(bombs.includes(i - 1)){
                 ++total
             }
-            if(bombs.includes(i - 19)){
+            if(bombs.includes(i - 21)){
                 ++total
             }
             if(bombs.includes(i - 20)){
@@ -167,11 +144,36 @@ function generateBombs() {
             if(bombs.includes(i - 1)){
                 ++total
             }
+        } else {
+            if(bombs.includes(i + 1)){
+                ++total
+            }
+            if(bombs.includes(i + 19)){
+                ++total
+            }
+            if(bombs.includes(i + 20)){
+                ++total
+            }
+            if(bombs.includes(i + 21)){
+                ++total
+            }
+            if(bombs.includes(i - 1)){
+                ++total
+            }
+            if(bombs.includes(i - 19)){
+                ++total
+            }
+            if(bombs.includes(i - 20)){
+                ++total
+            }
+            if(bombs.includes(i - 21)){
+                ++total
+            }
         }
-        //document.getElementById(i).innerText = total
+
+        document.getElementById(i).innerText = total
         bombsNear[i] = total;
     }
-
 }
 
 
@@ -184,14 +186,16 @@ function passTime() {
 function getButtonId(button) {
     return button.getAttribute("id")
 }
-
-function checkForBomb() {
-    const allButtons = Array.from(game.querySelectorAll(".buttons"))
-    allButtons.forEach(button => {
-        button.addEventListener('click', e => {
+const gameButtons = Array.from(game.querySelectorAll(".buttons"))
+gameButtons.forEach(button => {
+    button.addEventListener("click", e =>{
+        var buttonId = getButtonId(button)
+        checkForBomb(buttonId)
+    })
+})
+function checkForBomb(buttonId) {
             var isBomb = false
-            var buttonId = parseInt(getButtonId(button)) 
-            for(var i = 1; i < bombs.length; ++i) {
+            for(var i = 0; i < bombs.length; ++i) {
                 if(buttonId == bombs[i]) {
                     isBomb = true;
                     break;
@@ -208,13 +212,14 @@ function checkForBomb() {
             } else {
                 if(bombsNear[buttonId] != 0){
                     document.getElementById(buttonId).innerText = bombsNear[buttonId]
+                    document.getElementById(buttonId).disabled = true
+                } else {
                 }
+                
             }
-        })
-    })  
-    console.log(bombs)
-    console.log(allPossibleButtons)
-    console.log(bombsNear)
+
+    console.log(bombsNear[buttonId])
+    console.log(buttonId)
 }
 
 function addMark() {
@@ -223,6 +228,7 @@ function addMark() {
         button.addEventListener("contextmenu", e =>{
             var buttonId = getButtonId(button)
             document.getElementById(buttonId).style.backgroundColor = 'yellow'
+            flags.push(buttonId)
         })
     })
 }
